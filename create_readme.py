173d3ac34ad169207b8aacfeeeeb69e748efd91f
hoshianaaa <ryotaro.hoshina@gmail.com>
2023-01-15 19:46:34 +0900

    update

--- create_readme.py
+++ create_readme.py
@@ -1,8 +1,8 @@
 import glob
 
-files = glob.glob("./*")
-for file in files:
-    ex = file.split(".")[-1]
-    if ((ex == "png") or (ex == "jpg")):
-      print(file)
-
+with open('README.md', 'w') as f:
+  files = glob.glob("./*")
+  for file in files:
+      ex = file.split(".")[-1]
+      if ((ex == "png") or (ex == "jpg")):
+        print("![](" + file + ")",file=f)
