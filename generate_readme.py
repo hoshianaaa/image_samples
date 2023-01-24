807615d28308a6ed46f69a5960735ad20deee501
hoshianaaa <ryotaro.hoshina@gmail.com>
2023-01-24 21:07:22 +0900

    update

--- generate_readme.py
+++ generate_readme.py
@@ -1,9 +1,10 @@
 import glob
 
 with open('README.md', 'w') as f:
-  files = glob.glob("./*")
+  files = sorted(glob.glob("./*"))
   for file in files:
       ex = file.split(".")[-1]
       if ((ex == "png") or (ex == "jpg")):
+#        print(file)
         print("- " + file,file=f)
         print("![](" + file + ")",file=f)
